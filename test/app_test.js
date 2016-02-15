afd0284c3243e62ff92e27d18ef102e8bbabaf0f
David Josefson <david.josefson@gmail.com>
2016-02-15 16:01:49 +0100

    Added dummy test file

---
+++ test/app_test.js
@@ -0,0 +1,9 @@
+/* Non-complete test file for app.js */
+
+var expect = require('chai').expect;
+
+describe('Testing something simple', function()  { 
+  it('should be true when it is true', function()  {
+    expect(true).to.be.true;
+  });
+});
