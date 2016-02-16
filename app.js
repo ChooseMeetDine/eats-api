61e977dd1014f509a0e5b90a52ad71bdb5dcfd50
David Josefson <david.josefson@gmail.com>
2016-02-16 09:04:20 +0100

    Added gitignored .env-file for env-variables

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+// Exempelanvändning av environment variables som ligger i .env
+console.log(process.env.MONGO_DB_USER);
+
 app.get('/', function(req, res) {
   res.send('<p>Du gick till rooten i API:et</p>');
 });
