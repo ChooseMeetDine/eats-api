21f5b40873fc33ceee8ee2f281c78c4f8437c1ed
David Josefson <david.josefson@gmail.com>
2016-02-22 12:15:46 +0100

    Re-added previously disabled code

--- app.js
+++ app.js
@@ -1,40 +1,41 @@
 var app = require('express')();
 var http = require('http').Server(app);
-// var io = require('socket.io')(http);
-// var aglio = require('aglio');
+var io = require('socket.io')(http);
+var aglio = require('aglio');
 
 app.get('/', function(req, res) {
   res.send('<p>Du gick till rooten i API:et och här är env-variabeln MONGO_DB_USER i .env: ' +
     process.env.MONGO_DB_USER + '</p>');
 });
 
-// app.get('/docs', function(req, res) {
-//   var options = {
-//     themeTemplate: 'default',
-//     locals: {
-//       myVariable: 125
-//     }
-//   };
-//
-//   aglio.renderFile('./public/docs/README.apib', './public/docs/documentation.html', options,
-//     function(err, warnings) {
-//       if (err) {
-//         return console.log(err);
-//       }
-//       if (warnings) {
-//         console.log(warnings);
-//       }
-//       res.sendFile(__dirname + '/public/docs/documentation.html');
-//     });
-// });
+app.get('/docs', function(req, res) {
+  var options = {
+    themeTemplate: 'default',
+    locals: {
+      myVariable: 125
+    }
+  };
 
-// io.on('connection', function() {
-//   console.log('Someone connected to the API via socketIO!');
-// });
+  aglio.renderFile('./public/docs/README.apib', './public/docs/documentation.html', options,
+    function(err, warnings) {
+      if (err) {
+        return console.log(err);
+      }
+      if (warnings) {
+        console.log(warnings);
+      }
+      res.sendFile(__dirname + '/public/docs/documentation.html');
+    });
+});
+
+io.on('connection', function() {
+  console.log('Someone connected to the API via socketIO!');
+});
+
 var port = process.env.PORT || 3000;
 
 http.listen(port, function() {
   console.log('Eats API-server listening on port 3001..');
 });
 
-// module.exports = app;
+module.exports = app;
