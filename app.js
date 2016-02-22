ad20f95f6ba4afee890c2c07ad5ae554c660fb76
David Josefson <david.josefson@gmail.com>
2016-02-22 11:49:51 +0100

    Removed code that might make heroku fail and fixed env-port

--- app.js
+++ app.js
@@ -1,39 +1,39 @@
 var app = require('express')();
-var https = require('https').Server(app);
-var io = require('socket.io')(https);
-var aglio = require('aglio');
+var http = require('http').Server(app);
+// var io = require('socket.io')(http);
+// var aglio = require('aglio');
 
 app.get('/', function(req, res) {
   res.send('<p>Du gick till rooten i API:et och här är env-variabeln MONGO_DB_USER i .env: ' +
     process.env.MONGO_DB_USER + '</p>');
 });
 
-app.get('/docs', function(req, res) {
-  var options = {
-    themeTemplate: 'default',
-    locals: {
-      myVariable: 125
-    }
-  };
+// app.get('/docs', function(req, res) {
+//   var options = {
+//     themeTemplate: 'default',
+//     locals: {
+//       myVariable: 125
+//     }
+//   };
+//
+//   aglio.renderFile('./public/docs/README.apib', './public/docs/documentation.html', options,
+//     function(err, warnings) {
+//       if (err) {
+//         return console.log(err);
+//       }
+//       if (warnings) {
+//         console.log(warnings);
+//       }
+//       res.sendFile(__dirname + '/public/docs/documentation.html');
+//     });
+// });
 
-  aglio.renderFile('./public/docs/README.apib', './public/docs/documentation.html', options,
-    function(err, warnings) {
-      if (err) {
-        return console.log(err);
-      }
-      if (warnings) {
-        console.log(warnings);
-      }
-      res.sendFile(__dirname + '/public/docs/documentation.html');
-    });
-});
+// io.on('connection', function() {
+//   console.log('Someone connected to the API via socketIO!');
+// });
 
-io.on('connection', function() {
-  console.log('Someone connected to the API via socketIO!');
-});
-
-https.listen(3001, function() {
+http.listen(process.env.PORT || 3001, function() {
   console.log('Eats API-server listening on port 3001..');
 });
 
-module.exports = app;
+// module.exports = app;
