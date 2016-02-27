685c697e021401e3d272d0f6e8279514a00f5daa
Agetorp <elias.agetorp@gmail.com>
2016-02-27 10:58:17 +0100

    Added isvalid and bodyparser as dependencies

--- app.js
+++ app.js
@@ -1,70 +1,24 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var aglio = require('aglio');
+var routes = require('./app/routes/index');
+var bodyParser = require('body-parser');
 
-//Fake endpoint
-app.get('/users', function (req, res) {
-  res.json({
-    name: 'Musse',
-    age: 30
-  });
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-//----------------------------------------
-app.get('/', function (req, res) {
-  res.send('<p>Du gick till rooten i API:et och här är env-variabeln MONGO_DB_USER i .env: ' +
-    process.env.MONGO_DB_USER + '</p>');
-});
-
-app.get('/docs', function (req, res) {
-  var options = {
-    themeTemplate: 'default',
-    locals: {
-      myVariable: 125
-    }
-  };
-
-  aglio.renderFile('./public/docs/README.apib', './public/docs/documentation.html', options,
-    function (err, warnings) {
-      if (err) {
-        return console.log(err);
-      }
-      if (warnings) {
-        //console.log(warnings);
-      }
-      res.sendFile(__dirname + '/public/docs/documentation.html');
-    });
-});
+app.use('/', routes);
 
 
-app.get('/goaldocs', function (req, res) {
-  var options = {
-    themeTemplate: 'default',
-    locals: {
-      myVariable: 125
-    }
-  };
-
-  aglio.renderFile('./public/docs/GOALS.apib', './public/docs/goaldocs.html', options,
-    function (err, warnings) {
-      if (err) {
-        return console.log(err);
-      }
-      if (warnings) {
-        //console.log(warnings);
-      }
-      res.sendFile(__dirname + '/public/docs/goaldocs.html');
-    });
-});
-
-io.on('connection', function () {
+io.on('connection', function() {
   console.log('Someone connected to the API via socketIO!');
 });
 
 var port = process.env.PORT || 3001;
 
-http.listen(port, function () {
+http.listen(port, function() {
   console.log('Eats API-server listening on port ' + port);
 });
 
