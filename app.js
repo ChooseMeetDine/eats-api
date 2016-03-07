48b3cdbb9e6f8400204441e020e23f1344b16e84
John George <bravelygeorge@gmail.com>
2016-03-07 10:13:55 +0100

    added stormpath autentication fucntionality to API

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var aglio = require('aglio');
+var stormpath = require('express-stormpath');
+var knex = require('./app/shared/knex');
+
+app.use(stormpath.init(app, {
+  website: true
+}));
 
 //Fake endpoint
 app.get('/users', function (req, res) {
@@ -11,10 +17,28 @@ app.get('/users', function (req, res) {
   });
 });
 
+app.get('/loggaut', stormpath.getUser, function (req, res) {
+  if (req.user) {
+    res.sendFile(__dirname + '/public/loggaut.html');
+  } else {
+    res.send('Not logged in');
+  }
+});
+
 //----------------------------------------
-app.get('/', function (req, res) {
-  res.send('<p>Du gick till rooten i API:et och här är env-variabeln MONGO_DB_USER i .env: ' +
-    process.env.MONGO_DB_USER + '</p>');
+app.get('/', stormpath.getUser, function (req, res) {
+  if (req.user) {
+    //res.send('Hello, ' + req.user.email);
+    knex.select('email').from('user').where('email', '=', req.user.email)
+      .then(function (result) {
+        res.send(result);
+      }).catch(function (error) {
+        res.send(error);
+      });
+  } else {
+    res.send('<p>Du gick till rooten i API:et och här är env-variabeln MONGO_DB_USER i .env: ' +
+      process.env.MONGO_DB_USER + '</p>');
+  }
 });
 
 app.get('/docs', function (req, res) {
