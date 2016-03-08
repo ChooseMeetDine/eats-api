2d780f14630434f231a38567ae92845451bf9fca
David Josefson <david.josefson@gmail.com>
2016-03-08 09:02:32 +0100

    Moved code and removed annoying eslint error (function declaration order)

--- app/datahandlers/polls.js
+++ app/datahandlers/polls.js
@@ -2,16 +2,25 @@ var knex = require('../shared/knex');
 var pollsDatahandler = {};
 var Promise = require('bluebird');
 
-Promise.config({
-  // Enable warnings
-  warnings: true,
-  // Enable long stack traces
-  longStackTraces: true,
-  // Enable cancellation
-  cancellation: true,
-  // Enable monitoring
-  monitoring: true
-});
+pollsDatahandler.post = function(req) {
+  // 0. Kolla hur man gör transactions!
+  // 1. INSERT poll data i 'polls' RETURNING 'id'
+  // 2. INSERT user data i 'poll_users' mha poll-idt
+  // 3. INSERT restaurant data i 'restaurant_polls' mha poll-idt
+  // 4. SELECT data från 'polls', 'poll_users', 'restaurant_polls', 'groups'
+  // 5. skapa ett JSON-API-svar utifrån SELECT-datan
+  // 6. returnera JSON-API-svaret
+
+  return knex.transaction(function(trx) {
+    return insertPoll(trx, req)
+      .then(function(pollid) {
+        return Promise.join(
+          insertRestaurants(trx, pollid, req),
+          insertUsers(trx, pollid, req)
+        );
+      });
+  });
+};
 
 var insertPoll = function(trx, req) {
   return trx('poll')
@@ -45,25 +54,8 @@ var insertRestaurants = function(trx, pollid, req) {
   });
 };
 
-pollsDatahandler.post = function(req) {
-  // 0. Kolla hur man gör transactions!
-  // 1. INSERT poll data i 'polls' RETURNING 'id'
-  // 2. INSERT user data i 'poll_users' mha poll-idt
-  // 3. INSERT restaurant data i 'restaurant_polls' mha poll-idt
-  // 4. SELECT data från 'polls', 'poll_users', 'restaurant_polls', 'groups'
-  // 5. skapa ett JSON-API-svar utifrån SELECT-datan
-  // 6. returnera JSON-API-svaret
 
-  return knex.transaction(function(trx) {
-    return insertPoll(trx, req);
-    // .then(function(pollid) {
-    //   return Promise.join(
-    //     insertRestaurants(trx, pollid, req),
-    //     insertUsers(trx, pollid, req)
-    //   );
-    // });
-  });
 
-};
+
 
 module.exports = pollsDatahandler;
