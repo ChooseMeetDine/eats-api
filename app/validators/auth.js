b0c1511c52c43c772667d148d6e85b078abf4666
John George <bravelygeorge@gmail.com>
2016-03-14 10:56:53 +0100

    adjusted jwt verifyer to only listen to x-access-token header

--- app/validators/auth.js
+++ app/validators/auth.js
@@ -11,7 +11,7 @@ var auth = {};
 
 auth.validate = function(req, res, next) {
 
-  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+  var token = req.headers['x-access-token'];
 
   if (token) {
     jwt.verify(token, cert, function(err, decoded) {
