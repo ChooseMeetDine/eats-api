129ffff46d3a1a18a57f3ef2901809e9fa27cf94
Agetorp <elias.agetorp@gmail.com>
2016-03-14 08:55:04 +0100

    Added vote-relation to JSON-API response

--- app/datahandlers/polls.js
+++ app/datahandlers/polls.js
@@ -66,8 +66,9 @@ var createPollPostResponse = function(res, pollId, req) {
     selectPollUsersData(pollId, req),
     selectRestaurantPollsData(pollId),
     selectGroupData(pollId, req),
-    selectCreatorData(pollId)
-  ).spread(function(poll, users, restaurants, group, creator) {
+    selectCreatorData(pollId),
+    selectVotesData(pollId)
+  ).spread(function(poll, users, restaurants, group, creator, votes) {
     var i;
     var response = new responseModule(poll);
     response.addRelation(creator);
@@ -75,8 +76,10 @@ var createPollPostResponse = function(res, pollId, req) {
       response.addRelation(users[i]);
     }
     for (i = 0; i < restaurants.length; i++) {
-      console.log(restaurants[i]);
       response.addRelation(restaurants[i]);
+    }
+    for (i = 0; i < votes.length; i++) {
+      response.addRelation(votes[i]);
     }
     if (group) {
       response.addRelation(group);
@@ -89,7 +92,7 @@ var createPollPostResponse = function(res, pollId, req) {
 };
 
 var selectPollData = function(pollId) {
-  return knex.select('id', 'creator_id as creator', 'name', 'expires',
+  return knex.select('id', 'name', 'expires',
       'group_id as group', 'allow_new_restaurants as allowNewRestaurants')
     .from('poll')
     .where('id', pollId.toString())
@@ -110,8 +113,6 @@ var selectCreatorData = function(pollId) {
     })
     .where('poll.id', pollId.toString())
     .then(function(res) {
-      console.log('resasdasdasd');
-      console.log(res);
       return {
         type: 'user',
         relation: 'creator',
@@ -183,6 +184,55 @@ var selectGroupData = function(pollId, req) {
         resource: 'groups',
         data: res[0]
       };
+    });
+};
+
+var selectVotesData = function(pollId) {
+  return knex.select('id', 'poll_id', 'user_id', 'restaurant_id')
+    .from('vote')
+    .where('vote.poll_id', pollId.toString())
+    .then(function(res) {
+
+      var votes = [];
+      for (var i = 0; i < res.length; i++) {
+        var voteRelation = {
+          data: {
+            id: res[i].id,
+          },
+          relation: 'votes',
+          multiple: true,
+          type: 'vote',
+          resource: 'votes',
+          relationships: [{
+            data: {
+              id: res[i].user_id
+            },
+            relation: 'user',
+            multiple: false,
+            type: 'user',
+            resource: 'users',
+          }, {
+            data: {
+              id: res[0].restaurant_id
+            },
+            relation: 'restaurant',
+            multiple: false,
+            type: 'restaurant',
+            resource: 'restaurants',
+          }, {
+            data: {
+              id: res[i].poll_id
+            },
+            relation: 'poll',
+            multiple: false,
+            type: 'poll',
+            resource: 'polls',
+          }]
+        };
+
+        votes.push(voteRelation);
+      }
+      return votes;
     });
 };
 
