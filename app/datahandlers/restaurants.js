cf77eedd9396c6f0656b60ad79b11803e1f43ff9
Agetorp <elias.agetorp@gmail.com>
2016-03-14 17:54:37 +0100

    Moved all queries for post restaurant to a shared file

--- app/datahandlers/restaurants.js
+++ app/datahandlers/restaurants.js
@@ -1,8 +1,8 @@
-var knex = require('../shared/knex');
-var pollsDatahandler = {};
+var knex = require('../shared/database/knex');
+var restaurantDatahandler = {};
 var Promise = require('bluebird');
 var responseModule = require('../json_api/json_api');
-var _ = require('underscore');
+var queries = require('../shared/database/sql_queries/restaurants');
 
 // Handles POST requests to endpoint /restaurants
 // Inserts data to database with a transaction and then selects data based on the
@@ -10,7 +10,7 @@ var _ = require('underscore');
 //
 // Error handling is handled in the two promises separately, to be able to distinguish what error
 // was thrown and to be able to return a useful message to the user.
-pollsDatahandler.post = function(req) {
+restaurantDatahandler.post = function(req) {
   req.validUser = 10; //TODO: Remove once auth works
 
   return executeInsertionTransaction(req)
@@ -24,11 +24,11 @@ pollsDatahandler.post = function(req) {
 // for them to add queries to that object (just a way to split this function to smaller parts)
 var executeInsertionTransaction = function(req) {
   return knex.transaction(function(trx) {
-      return insertRestaurant(trx, req) // returns the restaurant id for the rest of the inserts
+      return queries.insertRestaurant(trx, req) // returns the restaurant id for following inserts
         .then(function(restaurantID) {
           return Promise.join(
-              insertCategories(trx, req, restaurantID), // two more inserts to other tables
-              insertRating(trx, req, restaurantID)
+              queries.insertCategories(trx, req, restaurantID), // two more inserts to other tables
+              queries.insertRating(trx, req, restaurantID)
             )
             .then(function() {
               return Promise.resolve(restaurantID);
@@ -42,15 +42,15 @@ var executeInsertionTransaction = function(req) {
 };
 
 // Executes several SELECT queries to the database and creates an object for each type of data
-// returned, which is used to create a JSON-API-poll object and add relations to it
+// returned, which is used to create a JSON-API-restaurant object and add relations to it
 var createRestaurantPostResponse = function(restaurantID) {
   return Promise.join( // run all SELECTs
-    selectRestaurantData(restaurantID), // returns a knex-select-promise
-    selectCreatorData(restaurantID),
-    selectRatingData(restaurantID),
-    selectCategoryData(restaurantID),
-    selectNumberOfPollsData(restaurantID),
-    selectNumberOfPollsWon(restaurantID)
+    queries.selectRestaurantData(restaurantID), // returns a knex-select-promise
+    queries.selectCreatorData(restaurantID),
+    queries.selectRatingData(restaurantID),
+    queries.selectCategoryData(restaurantID),
+    queries.selectNumberOfPollsData(restaurantID),
+    queries.selectNumberOfPollsWon(restaurantID)
   ).spread(function(restaurant, creator, rating, categories, pollCount, wonCount) {
     var i;
     restaurant.data.rating = rating; //Rating is just a number for now
@@ -64,140 +64,8 @@ var createRestaurantPostResponse = function(restaurantID) {
     return response;
   }).catch(function(err) {
     console.log(err.stack);
-    return Promise.reject(new Error('Could insert but not retrieve poll data from database'));
+    return Promise.reject(new Error('Could insert but not retrieve restaurant data from database'));
   });
 };
 
-// Takes an object for an ongoing transaction and runs an INSERT query "on" that object
-var insertRestaurant = function(trx, req) {
-  return trx('restaurant')
-    .insert({
-      creator_id: req.validUser,
-      name: req.validBody.name,
-      info: req.validBody.info,
-      lat: req.validBody.lat,
-      lng: req.validBody.lng,
-      price_rate: req.validBody.priceRate,
-      photo: req.validBody.photo,
-      temporary: req.validBody.temporary,
-      created: knex.raw('now()')
-    })
-    .returning('id');
-};
-
-var insertCategories = function(trx, req, restaurantID) {
-  return Promise.map(req.validBody.categories, function(categoryID) {
-    return trx.insert({
-      restaurant_id: restaurantID.toString(),
-      category_id: categoryID,
-    }).into('restaurant_categories');
-  });
-};
-
-var insertRating = function(trx, req, restaurantID) {
-  if (!req.validBody.rating) {
-    return Promise.resolve();
-  }
-  return trx.insert({
-    rater_id: req.validUser,
-    rating: req.validBody.rating,
-    restaurant_id: restaurantID.toString(),
-    created: knex.raw('now()')
-  }).into('rating');
-};
-
-var selectRestaurantData = function(restaurantID) {
-  return knex.select('id', 'name', 'lat', 'created', 'info', 'photo', 'temporary',
-      'lng', 'price_rate as priceRate')
-    .from('restaurant')
-    .where('id', restaurantID.toString())
-    .then(function(res) {
-      return {
-        type: 'restaurant',
-        resource: 'restaurants',
-        data: res[0]
-      };
-    });
-};
-
-var selectCreatorData = function(restaurantID) {
-  return knex.select('user.id', 'user.name', 'user.photo', 'anon')
-    .from('user')
-    .join('restaurant', {
-      'restaurant.creator_id': 'user.id'
-    })
-    .where('restaurant.id', restaurantID.toString())
-    .then(function(res) {
-      return {
-        type: 'user',
-        relation: 'creator',
-        multiple: false,
-        resource: 'users',
-        data: res[0]
-      };
-    });
-};
-
-var selectRatingData = function(restaurantID) {
-  return knex.pluck('rating')
-    .from('rating')
-    .where('restaurant_id', restaurantID.toString())
-    .then(function(res) {
-      var sum = 0;
-      for (var i = 0; i < res.length; i++) {
-        sum += res[i];
-      }
-      var avg = sum / res.length;
-      return avg.toFixed(1);
-    });
-};
-
-var selectCategoryData = function(restaurantID) {
-  return knex.select('id', 'type')
-    .from('restaurant_categories')
-    .join('category', 'restaurant_categories.category_id', 'category.id')
-    .where('restaurant_categories.restaurant_id', restaurantID.toString())
-    .then(function(res) {
-      var categories = [];
-      for (var i = 0; i < res.length; i++) {
-        var category = {
-          relation: 'categories',
-          multiple: true,
-          type: 'category',
-          resource: 'categories',
-          data: res[i]
-        };
-        categories.push(category);
-      }
-      return categories;
-    });
-};
-
-var selectNumberOfPollsData = function(restaurantID) {
-  return knex.select('restaurant_id')
-    .from('restaurant_polls')
-    .where('restaurant_id', restaurantID.toString())
-    .then(function(res) {
-      return res.length;
-    });
-};
-
-var selectNumberOfPollsWon = function(restaurantID) {
-  //This query is to complicated for me to write with knex. Hence the long string..ss
-  var query = 'select count(*) from ' +
-    '(select max(restaurant_id) as restaurant_id, poll_id from ' +
-    '(select count(restaurant_id), poll_id, restaurant_id from ' +
-    '(select vote.poll_id, restaurant_id, user_id from ' +
-    '(select poll_id from restaurant_polls where restaurant_id = ' +
-    restaurantID.toString() + ' ) as t1 ' +
-    'join vote on vote.poll_id = t1.poll_id ' +
-    'join poll on t1.poll_id = poll.id where poll.expires > now() ' +
-    ') as t2 group by poll_id, restaurant_id ' +
-    ') as t3 group by poll_id ' +
-    ') as t4 where restaurant_id = ' + restaurantID.toString();
-  return knex.raw(query)
-    .then(function(res) {
-      return res.rows[0].count;
-    });
-};
-module.exports = pollsDatahandler;
+module.exports = restaurantDatahandler;
---
+++ app/shared/database/sql_queries/restaurants.js
@@ -0,0 +1,138 @@
+var knex = require('../knex');
+var Promise = require('bluebird');
+var restaurantsQueries = {};
+
+// Takes an object for an ongoing transaction and runs an INSERT query "on" that object
+restaurantsQueries.insertRestaurant = function(trx, req) {
+  return trx('restaurant')
+    .insert({
+      creator_id: req.validUser,
+      name: req.validBody.name,
+      info: req.validBody.info,
+      lat: req.validBody.lat,
+      lng: req.validBody.lng,
+      price_rate: req.validBody.priceRate,
+      photo: req.validBody.photo,
+      temporary: req.validBody.temporary,
+      created: knex.raw('now()')
+    })
+    .returning('id');
+};
+
+restaurantsQueries.insertCategories = function(trx, req, restaurantID) {
+  return Promise.map(req.validBody.categories, function(categoryID) {
+    return trx.insert({
+      restaurant_id: restaurantID.toString(),
+      category_id: categoryID,
+    }).into('restaurant_categories');
+  });
+};
+
+restaurantsQueries.insertRating = function(trx, req, restaurantID) {
+  if (!req.validBody.rating) {
+    return Promise.resolve();
+  }
+  return trx.insert({
+    rater_id: req.validUser,
+    rating: req.validBody.rating,
+    restaurant_id: restaurantID.toString(),
+    created: knex.raw('now()')
+  }).into('rating');
+};
+
+restaurantsQueries.selectRestaurantData = function(restaurantID) {
+  return knex.select('id', 'name', 'lat', 'created', 'info', 'photo', 'temporary',
+      'lng', 'price_rate as priceRate')
+    .from('restaurant')
+    .where('id', restaurantID.toString())
+    .then(function(res) {
+      return {
+        type: 'restaurant',
+        resource: 'restaurants',
+        data: res[0]
+      };
+    });
+};
+
+restaurantsQueries.selectCreatorData = function(restaurantID) {
+  return knex.select('user.id', 'user.name', 'user.photo', 'anon')
+    .from('user')
+    .join('restaurant', {
+      'restaurant.creator_id': 'user.id'
+    })
+    .where('restaurant.id', restaurantID.toString())
+    .then(function(res) {
+      return {
+        type: 'user',
+        relation: 'creator',
+        multiple: false,
+        resource: 'users',
+        data: res[0]
+      };
+    });
+};
+
+restaurantsQueries.selectRatingData = function(restaurantID) {
+  return knex.pluck('rating')
+    .from('rating')
+    .where('restaurant_id', restaurantID.toString())
+    .then(function(res) {
+      var sum = 0;
+      for (var i = 0; i < res.length; i++) {
+        sum += res[i];
+      }
+      var avg = sum / res.length;
+      return avg.toFixed(1);
+    });
+};
+
+restaurantsQueries.selectCategoryData = function(restaurantID) {
+  return knex.select('id', 'type')
+    .from('restaurant_categories')
+    .join('category', 'restaurant_categories.category_id', 'category.id')
+    .where('restaurant_categories.restaurant_id', restaurantID.toString())
+    .then(function(res) {
+      var categories = [];
+      for (var i = 0; i < res.length; i++) {
+        var category = {
+          relation: 'categories',
+          multiple: true,
+          type: 'category',
+          resource: 'categories',
+          data: res[i]
+        };
+        categories.push(category);
+      }
+      return categories;
+    });
+};
+
+restaurantsQueries.selectNumberOfPollsData = function(restaurantID) {
+  return knex.select('restaurant_id')
+    .from('restaurant_polls')
+    .where('restaurant_id', restaurantID.toString())
+    .then(function(res) {
+      return res.length;
+    });
+};
+
+restaurantsQueries.selectNumberOfPollsWon = function(restaurantID) {
+  //This query is to complicated for me to write with knex. Hence the long string..ss
+  var query = 'select count(*) from ' +
+    '(select max(restaurant_id) as restaurant_id, poll_id from ' +
+    '(select count(restaurant_id), poll_id, restaurant_id from ' +
+    '(select vote.poll_id, restaurant_id, user_id from ' +
+    '(select poll_id from restaurant_polls where restaurant_id = ' +
+    restaurantID.toString() + ' ) as t1 ' +
+    'join vote on vote.poll_id = t1.poll_id ' +
+    'join poll on t1.poll_id = poll.id where poll.expires > now() ' +
+    ') as t2 group by poll_id, restaurant_id ' +
+    ') as t3 group by poll_id ' +
+    ') as t4 where restaurant_id = ' + restaurantID.toString();
+  return knex.raw(query)
+    .then(function(res) {
+      return res.rows[0].count;
+    });
+};
+
+module.exports = restaurantsQueries;
