87e03edeff5ac64eb610f65aeb5395a0e8147afd
Agetorp <elias.agetorp@gmail.com>
2016-03-15 20:58:08 +0100

    Added route for get poll id

--- app/routes/polls.js
+++ app/routes/polls.js
@@ -17,5 +17,20 @@ router.post('/', pollValidator.post, function(req, res) {
       });
     });
 });
+router.post('/', pollValidator.getID, function(req, res) {
+  return pollHandler
+    .getID(req)
+    .then(function(response) {
+      console.log('Sending data through HTTP... ' + JSON.stringify(response));
+      res.send(response);
+    })
+    .catch(function(err) {
+      res.status(500).send({
+        httpStatus: 500,
+        error: err.message,
+        stack: err.stack
+      });
+    });
+});
 
 module.exports = router;
