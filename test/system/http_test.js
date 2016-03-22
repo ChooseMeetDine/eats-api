76bbe47446acc5703fdcdf6311e1517a465c02be
Notika <nnotika@gmail.com>
2016-03-22 11:05:33 +0100

    added systemtests for GET/poll and GET/restaurants

--- test/system/http_test.js
+++ test/system/http_test.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 var request = require('supertest');
 var rewire = require('rewire');
 var app = rewire('../../app');
@@ -5,35 +6,379 @@ var expect = require('chai').expect;
 var chai = require('chai');
 chai.use(require('chai3-json-schema'));
 
-var jsonSchema = {
-  'title': 'user schema',
+var jsonSchemaPollGet = {
+  // schema is built according to standard Shcema Draft4 by using http://jsonschema.net/#/
+  // defined in API documentation json-format was translated to json-schema
+  // extra id-s which were added by service are deleted, required attributes were defined
+  '$schema': 'http://json-schema.org/draft-04/schema#',
   'type': 'object',
   'properties': {
-    'name': {
-      'type': 'string'
+    'data': {
+      'type': 'object',
+      'properties': {
+        'type': {
+          'type': 'string'
+        },
+        'id': {
+          'type': 'string'
+        },
+        'attributes': {
+          'type': 'object',
+          'properties': {
+            'name': {
+              'type': 'string'
+            },
+            'expires': {
+              'type': 'string'
+            },
+            'created': {
+              'type': 'string',
+              'required': true
+            },
+            'allowNewRestaurants': {
+              'type': 'boolean',
+              'required': true
+            }
+          },
+          'required': ['created', 'expires', 'allowNewRestaurants', 'name']
+        }
+      },
+      'required': [
+        'type',
+        'id',
+        'attributes'
+      ]
     },
-    'age': {
-      'type': 'number',
-      'minimum': 5
+    'relationships': {
+      'type': 'object',
+      'properties': {
+        'creator': {
+          'type': 'object',
+          'properties': {
+            'data': {
+              'type': 'object',
+              'properties': {
+                'type': {
+                  'type': 'string'
+                },
+                'id': {
+                  'type': 'string'
+                }
+              },
+              'required': ['type', 'id']
+            }
+          },
+          'required': ['data']
+        },
+        'restaurants': {
+          'type': 'object',
+          'properties': {
+            'data': {
+              'type': 'array',
+              'items': {
+                'type': 'object',
+                'properties': {
+                  'type': {
+                    'type': 'string'
+                  },
+                  'id': {
+                    'type': 'string'
+                  }
+                }
+              }
+            }
+          }
+        },
+        'users': {
+          'type': 'object',
+          'properties': {
+            'data': {
+              'type': 'array',
+              'items': {
+                'type': 'object',
+                'properties': {
+                  'type': {
+                    'type': 'string'
+                  },
+                  'id': {
+                    'type': 'string'
+                  }
+                }
+              }
+            }
+          }
+        },
+        'group': {
+          'type': 'object',
+          'properties': {
+            'data': {
+              'type': 'object',
+              'properties': {
+                'type': {
+                  'type': 'string'
+                },
+                'id': {
+                  'type': 'string'
+                }
+              }
+            }
+          }
+        }
+      },
+      'required': ['users', 'group', 'restaurants', 'creator']
+    },
+    'links': {
+      'type': 'object',
+      'properties': {
+        'self': {
+          'type': 'string'
+        }
+      }
+    },
+    'included': {
+      'type': 'array',
+      'items': [{
+        'type': 'object',
+        'properties': {
+          'type': {
+            'type': 'string'
+          },
+          'id': {
+            'type': 'string'
+          },
+          'attributes': {
+            'type': 'object',
+            'properties': {
+              'name': {
+                'type': 'string'
+              },
+              'latitude': {
+                'type': 'number'
+              },
+              'longitude': {
+                'type': 'number'
+              },
+              'temporary': {
+                'type': 'boolean'
+              }
+            }
+          },
+          'links': {
+            'type': 'object',
+            'properties': {
+              'self': {
+                'type': 'string'
+              }
+            }
+          }
+        }
+      }, {
+        'type': 'object',
+        'properties': {
+          'type': {
+            'type': 'string'
+          },
+          'id': {
+            'type': 'string'
+          },
+          'attributes': {
+            'type': 'object',
+            'properties': {
+              'name': {
+                'type': 'string'
+              },
+              'photo': {
+                'type': 'string'
+              },
+              'anonymous': {
+                'type': 'boolean'
+              }
+            }
+          },
+          'links': {
+            'type': 'object',
+            'properties': {
+              'self': {
+                'type': 'string'
+              }
+            }
+          }
+        }
+      }, {
+        'type': 'object',
+        'properties': {
+          'type': {
+            'type': 'string'
+          },
+          'id': {
+            'type': 'string'
+          },
+          'attributes': {
+            'type': 'object',
+            'properties': {
+              'name': {
+                'type': 'string'
+              }
+            }
+          },
+          'links': {
+            'type': 'object',
+            'properties': {
+              'self': {
+                'type': 'string'
+              }
+            }
+          }
+        }
+      }]
     }
-  }
+  },
+  'additionalProperties': false,
+  'required': [
+    'data',
+    'relationships',
+    'links',
+    'included'
+  ]
 };
-
+var jsonSchemaRestaurantGet = {
+  // schema is built according to standard Shcema Draft4 by using http://jsonschema.net/#/
+  // defined in API documentation json-format was translated to json-schema
+  // extra id-s which were added by service are deleted, required attributes were defined
+  '$schema': 'http://json-schema.org/draft-04/schema#',
+  'type': 'object',
+  'properties': {
+    'links': {
+      'id': 'links',
+      'type': 'object',
+      'properties': {
+        'self': {
+          'type': 'string'
+        }
+      },
+      'required': [
+        'self'
+      ]
+    },
+    'data': {
+      'type': 'array',
+      'items': {
+        'type': 'object',
+        'properties': {
+          'type': {
+            'type': 'string'
+          },
+          'id': {
+            'type': 'string'
+          },
+          'attributes': {
+            'type': 'object',
+            'properties': {
+              'name': {
+                'type': 'string'
+              },
+              'info': {
+                'type': 'string'
+              },
+              'photo': {
+                'id': 'photo',
+                'type': 'string'
+              },
+              'priceRate': {
+                'type': 'integer'
+              },
+              'rating': {
+                'type': 'integer'
+              },
+              'numberVotes': {
+                'type': 'integer'
+              },
+              'numberWonVotes': {
+                'type': 'integer'
+              },
+              'longitude': {
+                'type': 'number'
+              },
+              'latitude': {
+                'type': 'number'
+              },
+              'temporary': {
+                'type': 'boolean'
+              }
+            },
+            'required': ['name', 'info', 'longitude', 'latitude', 'numberVotes', 'numberWonVotes',
+              'rating', 'priceRate', 'photo'
+            ]
+          },
+          'relationships': {
+            'type': 'object',
+            'properties': {
+              'category': {
+                'type': 'object',
+                'properties': {
+                  'data': {
+                    'type': 'array',
+                    'items': {
+                      'type': 'object',
+                      'properties': {
+                        'type': {
+                          'type': 'string'
+                        },
+                        'id': {
+                          'type': 'string'
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          },
+          'links': {
+            'type': 'object',
+            'properties': {
+              'self': {
+                'type': 'string'
+              }
+            }
+          }
+        },
+        'required': ['type', 'id', 'attributes', 'relationships', 'links']
+      }
+    },
+    'included': {
+      'type': 'array',
+      'items': {
+        'type': 'object',
+        'properties': {
+          'type': {
+            'type': 'string'
+          },
+          'id': {
+            'type': 'string'
+          },
+          'attributes': {
+            'type': 'object',
+            'properties': {
+              'name': {
+                'type': 'string'
+              }
+            },
+            'required': ['name']
+          }
+        },
+        'required': ['type', 'id', 'attributes']
+      }
+    }
+  },
+  'required': [
+    'links',
+    'data',
+    'included'
+  ]
+};
 describe('Testing Eats-API HTTP requests', function() {
   describe('Testing endpoints', function() {
-    it('should return valid json data', function(done) {
-      request(app)
-        .get('/users')
-        .expect(200)
-        .expect('Content-Type', /json/)
-        .end(function(err, res) {
-          var response = JSON.parse(res.text);
-          expect(response).to.be.jsonSchema(jsonSchema);
-          done();
-        });
-    });
-
-
     it('Should load the root page for GET /', function(done) {
       request(app)
         .get('/')
@@ -49,7 +394,6 @@ describe('Testing Eats-API HTTP requests', function() {
         .expect(200)
         .end(done);
     });
-
     it('Should return API docs for GET /goaldocs', function(done) {
       request(app)
         .get('/goaldocs')
@@ -57,21 +401,97 @@ describe('Testing Eats-API HTTP requests', function() {
         .expect(200)
         .end(done);
     });
-    /*
-        it('POSTs a vote', function (done) {
-          request('http://localhost:3001')
-            .post('/vote')
-            .send({})
-            .expect(200)
-            .end(done);
+    it('should return valid json data for GET/polls', function(done) {
+      request(app)
+        .get('/polls')
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .end(function(err, res) {
+          var response = JSON.parse(res.text);
+          expect(response).to.be.jsonSchema(jsonSchemaPollGet);
+          done();
         });
-
-        it('PUTs ....', function (done) {
-          request(app).put('/../id').send({}).expect(200).end(done);
+    });
+    it('should return valid json data for GET/restaurants', function(done) {
+      request(app)
+        .get('/restaurants')
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .end(function(err, res) {
+          var response = JSON.parse(res.text);
+          expect(response).to.be.jsonSchema(jsonSchemaRestaurantGet);
+          done();
         });
+    });
+    // it('POSTs a poll', function(done) {
+    //   request(app)
+    //     .post('/polls')
+    //     .set('Content-Type', 'application/json')
+    //     .send({})
+    //     .end(function(err, res) {
+    //       var response = JSON.parse(res.text);
+    //       expect(response).to.deep.equal({
+    //         authentication: false,
+    //         message: 'You need to POST email and password',
+    //         token: false
+    //       });
+    //       done();
+    //     });
+    it('Should return valid data for POST /auth', function(done) {
+      var port = process.env.PORT || 3001;
+      console.log(port);
+      request('http://localhost:' + port.toString())
+        .post('/auth')
+        .send({
+          'email': 'musse@mail.se',
+          'password': 'password123'
+        })
+        .expect(200)
+        .end(function(err, res) {
+          //console.log(res.body);
+          var response = res.body;
+          console.log(response);
+          expect(response).to.be.jsonSchema({
+            'title': 'auth valid schema',
+            'type': 'object',
+            'properties': {
+              'authentication': {
+                'type': 'boolean'
+              },
+              'message': {
+                'type': 'string'
+              },
+              'token': {
+                'type': 'string'
+              }
+            }
+          });
+          done();
+        });
+    });
 
-        it('DELETEs ....', function (done) {
-          request(app).delete('/../id').expect(200).end(done);
-        });*/
+    it('Should return json response with false authentication', function(done) {
+      request(app)
+        .post('/auth')
+        .set('Content-Type', 'application/json')
+        .end(function(err, res) {
+          var response = JSON.parse(res.text);
+          expect(response).to.deep.equal({
+            authentication: false,
+            message: 'You need to POST email and password',
+            token: false
+          });
+          done();
+        });
+    });
+
+
+    /*  it('PUTs ....', function (done) {
+        request(app).put('/../id').send({}).expect(200).end(done);
+      });
+
+      it('DELETEs ....', function (done) {
+        request(app).delete('/../id').expect(200).end(done);
+      });*/
   });
 });
