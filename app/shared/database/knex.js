8b9d5e3dda56a83074457a1ba33c381d842267b3
Agetorp <elias.agetorp@gmail.com>
2016-03-24 14:33:06 +0100

    Changed back to old database to see if travis passes

--- app/shared/database/knex.js
+++ app/shared/database/knex.js
@@ -1,6 +1,13 @@
 var knex = require('knex')({
   client: 'pg',
-  connection: process.env.POSTGRES_CONNECTION,
+  connection: {
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASS,
+    database: process.env.POSTGRES_DB,
+    port: process.env.POSTGRES_PORT,
+    host: process.env.POSTGRES_HOST,
+    ssl: true
+  },
   searchPath: 'knex,public',
   pool: {
     min: 1,
