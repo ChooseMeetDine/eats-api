3ebe9d43ea8b610cdb5a3ca2d3f8148bd988894c
John George <bravelygeorge@gmail.com>
2016-03-29 15:18:09 +0200

    added anonymous user jwt generator POST /auth/anonymous

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -4,6 +4,7 @@ var knex = require('../shared/database/knex');
 var bodyParser = require('body-parser');
 var cert = process.env.JWTSECRET;
 var authRequest = require('../validators/auth');
+var moniker = require('moniker');
 
 router.use(bodyParser.urlencoded({
   extended: true
@@ -33,11 +34,11 @@ router.post('/', authRequest.checkData, function(req, res) {
 
         if (user.admin === true) {
           token = jwt.sign(user, cert, {
-            expiresIn: '168h' // expires in one week (168 hours)
+            expiresIn: '168h' //NOTE expires in one week (168 hours)
           });
         } else {
           token = jwt.sign(user, cert, {
-            expiresIn: '5m' // expires in 1 minute
+            expiresIn: '5m' //NOTE expires in 5 minutes
           });
         }
 
@@ -61,5 +62,43 @@ router.post('/', authRequest.checkData, function(req, res) {
       });
     });
 });
+
+router.get('/anonymous', function(req, res) {
+
+  var anonymousId;
+  var randomNameGenerator = moniker.choose();
+
+  knex.insert({
+      name: 'anonymous ' + randomNameGenerator,
+      last_login: knex.raw('now()'),
+      registration_date: knex.raw('now()'),
+      anon: true
+    })
+    .into('user')
+    .returning('id')
+    .then(function(result) {
+      console.log(result[0]);
+      anonymousId = result[0];
+    }).catch(function(error) {
+      console.log(error);
+    });
+
+  var anonymousUser = {
+    id: anonymousId,
+    anon: true,
+    role: 'anonymous'
+  };
+
+  var token = jwt.sign(anonymousUser, cert, {
+    expiresIn: '24h' //NOTE expires in 24h
+  });
+
+  res.json({
+    message: 'Welcome anonymous ' + randomNameGenerator,
+    token: token
+  });
+});
+
+console.log(moniker.choose());
 
 module.exports = router;
