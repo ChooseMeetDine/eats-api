3399725ed91c83dacf586d4093989b6932f16776
Agetorp <elias.agetorp@gmail.com>
2016-04-04 10:31:27 +0200

    Fixed auth-test

--- test/system/auth.js
+++ test/system/auth.js
@@ -1,39 +1,25 @@
+/**
+ * Tests are run from http_test.js
+ */
+
 var request = require('supertest');
 var chai = require('chai');
 var expect = chai.expect;
 chai.use(require('chai3-json-schema'));
 chai.tv4.banUnknown = true;
 
-module.exports = function(app, tokens) {
+module.exports = function(app) {
   describe('Testing auth endpoint', function() {
-    it('Should return json response for POST /auth', function(done) {
+    it('Should return error response for POST /auth without any body', function(done) {
       request(app)
         .post('/auth')
-        .expect(200)
-        .end(function(err, res) {
-          var response = JSON.parse(res.text);
-          expect(response).to.be.jsonSchema({
-            'title': 'auth post schema',
-            'type': 'object',
-            'properties': {
-              'authentication': {
-                'type': 'boolean'
-              },
-              'message': {
-                'type': 'string'
-              },
-              'token': {
-                'type': 'boolean'
-              }
-            }
-          });
-          done();
-        });
+        .expect(400, done);
     });
 
     it('Should return valid data for POST /auth', function(done) {
       request(app)
         .post('/auth')
+        .expect(200)
         .send({
           email: 'jenny@mail.se',
           password: 'password123'
@@ -56,7 +42,7 @@ module.exports = function(app, tokens) {
               }
             }
           });
-          done();
+          done(err);
         });
     });
 
