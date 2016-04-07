959d8271a202bbf0f0f1573b8047f8b43f22720c
David Josefson <david.josefson@gmail.com>
2016-04-07 13:06:04 +0200

    Wrote system tests for POST to /poll/:id/restaurants

--- test/system/polls.js
+++ test/system/polls.js
@@ -103,6 +103,97 @@ module.exports = function(app, tokens) {
           });
       });
     });
+    describe('Testing POST /polls/:id/restaurants', function() {
+      it('should return error when no token is used', function(done) {
+        request(app)
+          .post('/polls/1135/restaurants')
+          .send({})
+          .expect(403, done);
+      });
+      it('should return error when an invalid poll ID is used', function(done) {
+        request(app)
+          .post('/polls/9999/restaurants')
+          .send({})
+          .set('x-access-token', tokens.user3)
+          .expect(404, done);
+      });
+      it('should return error when the user is not a participant in the poll', function(done) {
+        request(app)
+          .post('/polls/1135/restaurants')
+          .send({})
+          .set('x-access-token', tokens.user2)
+          .expect(403, done);
+      });
+      it('should return error if poll has expired', function(done) {
+        request(app)
+          .post('/polls/1115/restaurants')
+          .send({})
+          .set('x-access-token', tokens.user3)
+          .expect(400, done);
+      });
+      it('should return error if the POST-body is invalid', function(done) {
+        request(app)
+          .post('/polls/1135/restaurants')
+          .send({
+            name: 'adsfasdf'
+          })
+          .set('x-access-token', tokens.user3)
+          .expect(400, done);
+      });
+      it('should return error if the restaurant is already added to the poll', function(done) {
+        request(app)
+          .post('/polls/1135/restaurants')
+          .send({
+            restaurantId: '1121'
+          })
+          .set('x-access-token', tokens.user3)
+          .expect(400, done);
+      });
+      it('should return error if the poll doesnt allow new restaurants', function(done) {
+        request(app)
+          .post('/polls/1115/restaurants')
+          .send({
+            restaurantId: '1151'
+          })
+          .set('x-access-token', tokens.user3)
+          .expect(400, done);
+      });
+
+      // TODO: Fixa gör ett nästlat GET-anrop mot /polls/:id för poll ID 1135 och kontrollera
+      // att restauranten verkligen är tillagd
+      // Exempelkod:
+      //
+      // request(app)
+      // .post()....
+      // .end(function(err, res){
+      //  expect(res.data.type).to.equal('restaurant');
+      //  var id = res.data.id;
+      //  request(app)
+      //  .get(/polls/:id)....
+      //  .end(function(err, res){
+      //    expect(schema)
+      //    var votes = []
+      //    for (let i = 0; i < res.data.relationships.restaurants.length; i++) {
+      //      votes.push(res.data.relationships.restaurants[i].id)
+      //    }
+      //    expect(restaurants.indexOf(id)).to.not.equal(-1);
+      //
+      // });
+      it('should return valid json for valid POST', function(done) {
+        request(app)
+          .post('/polls/1135/restaurants')
+          .send({
+            restaurantId: '1151'
+          })
+          .set('x-access-token', tokens.user3)
+          .expect(200)
+          .expect('Content-Type', /json/)
+          .end(function(err, res) {
+            expect(res.body).to.be.jsonSchema(jsonSchemaPollIdRestaurantsPost());
+            done(err);
+          });
+      });
+    });
   });
 
 };
@@ -152,6 +243,84 @@ var jsonSchemaPollIdVotePost = function() {
     'required': [
       'data',
       'links'
+    ]
+  };
+};
+
+var jsonSchemaPollIdRestaurantsPost = function()  {
+  return {
+    '$schema': 'http://json-schema.org/draft-04/schema#',
+    'type': 'object',
+    'properties': {
+      'links': {
+        'type': 'object',
+        'properties': {
+          'self': {
+            'type': 'string'
+          }
+        },
+        'required': [
+          'self'
+        ]
+      },
+      'data': {
+        'type': 'object',
+        'properties': {
+          'type': {
+            'type': 'string'
+          },
+          'id': {
+            'type': 'string'
+          },
+          'attributes': {
+            'type': 'object',
+            'properties': {
+              'name': {
+                'type': 'string'
+              },
+              'info': {
+                'type': ['string', 'null']
+              },
+              'photo': {
+                'id': 'photo',
+                'type': ['string', 'null']
+              },
+              'priceRate': {
+                'type': ['integer', 'null']
+              },
+              'rating': {
+                'type': ['integer', 'null']
+              },
+              'numberVotes': {
+                'type': 'integer'
+              },
+              'numberWonVotes': {
+                'type': 'integer'
+              },
+              'lng': {
+                'type': ['number', 'null']
+              },
+              'lat': {
+                'type': ['number', 'null']
+              },
+              'temporary': {
+                'type': 'boolean'
+              },
+              'status': {
+                'type': 'string'
+              }
+            },
+            'required': ['name', 'info', 'lng', 'lat',
+              'rating', 'priceRate', 'photo'
+            ]
+          }
+        },
+        'required': ['type', 'id', 'attributes']
+      },
+    },
+    'required': [
+      'links',
+      'data'
     ]
   };
 };
